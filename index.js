321d365b374b5f8e955492945c4b0d9421a5d30e
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-19 14:57:59 -0500

    Added triggers array

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ require("dotenv").config();
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login(process.env.BOTTOKEN);
+const replies = require("./replies.json");
 
 client.on("ready", e => {
     console.log("Bot is ready! 🙌");
 });
+
+const triggers = ["hello", "hi"];
+
+client.on("message", msg => {
+    if (triggers.includes(msg.content.toLowerCase()) && msg.channel.id == "801128717694271498") {
+        // msg.reply("fuck you! " + "🙌❤💕😊😎");
+        const idx = Math.floor(Math.random() * replies.length);
+        msg.channel.send(replies[idx]);
+    }
+});
+
+client.login(process.env.BOTTOKEN);
