652574565ad0cb80cea578d7b420bbe8b3c1533c
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-01-20 19:08:59 -0500

    It now checks for all files in 'commands' folder: no manual require

--- commands.js
+++ commands.js
@@ -1,22 +1,24 @@
-// TODO: check for files in commands dir and add them to commands object with node fs
+const fs = require("fs");
+const path = require("path");
 
 const { triggers } = require("./data.json");
 
-const hello = require("./commands/hello");
-const gif = require("./commands/gif");
-const dump = require("./commands/dump");
-const catFacts = require("./commands/catFacts");
-const dict = require("./commands/dict");
-const insult = require("./commands/insult");
+let commands = {};
 
-const commands = {
-    gif,
-    hello,
-    dump,
-    catFacts,
-    dict,
-    insult
-};
+const hello = require("./commands/hello");
+
+const dirPath = path.join(__dirname, "commands");
+
+fs.readdir(dirPath, (err, files) => {
+    if (err) {
+        return console.error("Unable to scan directory: " + err);
+    }
+    files.forEach(file => {
+        const name = file.split(".")[0];
+        const func = require("./commands/" + name);
+        commands[name] = func;
+    });
+});
 
 const commandHandler = async msg => {
     if (msg.channel.id == process.env.CHANNELID) {
